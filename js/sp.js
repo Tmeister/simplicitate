2944f559c46d0029b36a9a0ddc5df496714fc0d2
Tmeister <noone@tmeister.net>
2013-01-07 10:14:22 -0600

    Update carouFredSel to 6.1.0 and PL 2.3.8 && WP 3.5 CSS issues.
    Also remove all "$" for javascript using jQuery instead.

--- js/sp.js
+++ js/sp.js
@@ -1,9 +1,9 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function() {
 	
 	var options = '<option selected>Site Navigation..</option>';
 	
-	$('.main-nav').find('a').each(function () {
-		$this = $(this);
+	jQuery('.main-nav').find('a').each(function () {
+		$this = jQuery(this);
 	    var text = $this.text(),
 	        depth = $this.parent().parents('ul').length,
 	        depthChar = '',
@@ -13,26 +13,26 @@ jQuery(document).ready(function($) {
 	    options += '<option value="'+link+'">' + depthChar + text + '</option>';
 	});
 	
-	$('<select class="menu-mobile" />').append(options).appendTo( '.section-simplicitate-menu .content-pad' );
+	jQuery('<select class="menu-mobile" />').append(options).appendTo( '.section-simplicitate-menu .content-pad' );
 
-	$('.menu-mobile').change(function(e){
-		var $this = $(this);
+	jQuery('.menu-mobile').change(function(e){
+		var $this = jQuery(this);
 		console.log( $this.val() );
 		location.href = $this.val();
 	});
 
 
-	$(".full_img a").hover(function(){
-		$(this).find('img').stop().animate({opacity : '0.7'}, 200);
-		$(this).append('<div class="link"></div>');
-		$(this).find('.link').animate({opacity : '1', top : '0px'}, 200);
+	jQuery(".full_img a").hover(function(){
+		jQuery(this).find('img').stop().animate({opacity : '0.7'}, 200);
+		jQuery(this).append('<div class="link"></div>');
+		jQuery(this).find('.link').animate({opacity : '1', top : '0px'}, 200);
 
 	}, function(){
-		$(this).find('img').stop().animate({opacity : '1'}, 200);
-		$(this).find('.link').animate({opacity : '0', top : '200px'}, 200 ,function(){ 
-			$(this).remove(); 
+		jQuery(this).find('img').stop().animate({opacity : '1'}, 200);
+		jQuery(this).find('.link').animate({opacity : '0', top : '200px'}, 200 ,function(){ 
+			jQuery(this).remove(); 
 		});
-		$(this).find('.link').css({top: '-200px;'});
+		jQuery(this).find('.link').css({top: '-200px;'});
 	});
 
 });
